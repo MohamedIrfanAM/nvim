eea8712425a9bc948d3561bfd6dcb5362d8833ee
MohamedIrfanAM <i4irfan777@gmail.com>
2022-01-03 08:49:22 +0530

    snippet expansion and jumping has higher priority than lsp autocomplete

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -67,10 +67,10 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if luasnip.expandable() then
         luasnip.expand()
-      elseif cmp.visible() then
-        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
+      elseif cmp.visible() then
+        cmp.select_next_item()
       elseif check_backspace() then
         fallback()
       else
