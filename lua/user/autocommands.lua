af697bd0af0b5efc8dbd0695e49f25bfe973aab9
MohamedIrfanAM <i4irfan777@gmail.com>
2022-01-12 20:29:58 +0530

    autocommand for inserting cursor to solve() function in .cpp

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -10,9 +10,10 @@ vim.cmd [[
   augroup _cp
     autocmd!
     autocmd TextChanged *.ans[1-9],*.in[1-9] silent! %s/\r$/
+    autocmd InsertEnter * :let @/=""
     let last_cursor_found = 1
     autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | else |let last_cursor_found = 0 |endif
-    autocmd BufReadPost *.cpp if last_cursor_found == 0 | 18 call feedkeys("i\<right>\<right>")  | endif
+    autocmd BufReadPost *.cpp if last_cursor_found == 0 | call feedkeys("/solve\<CR>:\<BS>\<ESC>o") | endif
     autocmd BufWinEnter *.ans[1-9],*.in[1-9] set nobuflisted
   augroup end
 
