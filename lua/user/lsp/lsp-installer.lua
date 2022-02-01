ea492d8a3d29bff69a63d59a4247e7752d1ab2f9
MohamedIrfanAM <i4irfan777@gmail.com>
2022-02-01 15:20:44 +0530

    LSP install if not present

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,8 +3,20 @@ if not status_ok then
 	return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
+local servers = {
+  "sumneko_lua",
+  "clangd",
+  "texlab"
+}
+
+for _, name in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found and not server:is_installed() then
+    print("Installing " .. name)
+    server:install()
+  end
+end
+
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
@@ -21,8 +33,6 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", clangd_opts, opts)
 	 end
 
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
 end)
